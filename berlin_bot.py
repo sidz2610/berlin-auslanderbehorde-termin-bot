00b5f043b22ba70d8755d3af7505da29798ae07f
sidz2610 <sidz020.sd@gmail.com>
2023-06-02 18:19:54 +0530

    added code for permit english appointment

--- berlin_bot.py
+++ berlin_bot.py
@@ -16,6 +16,7 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+
 class WebDriver:
     def __init__(self):
         self._driver: webdriver.Chrome
@@ -24,17 +25,20 @@ class WebDriver:
     def __enter__(self) -> webdriver.Chrome:
         logging.info("Open browser")
         # some stuff that prevents us from being locked out
-        options = webdriver.ChromeOptions() 
+        options = webdriver.ChromeOptions()
         options.add_argument('--disable-blink-features=AutomationControlled')
         self._driver = webdriver.Chrome(options=options)
-        self._driver.implicitly_wait(self._implicit_wait_time) # seconds
-        self._driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-        self._driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+        self._driver.implicitly_wait(self._implicit_wait_time)  # seconds
+        self._driver.execute_script(
+            "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        self._driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+                                     "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
         return self._driver
 
     def __exit__(self, exc_type, exc_value, exc_tb):
         logging.info("Close browser")
         self._driver.quit()
+
 
 class BerlinBot:
     def __init__(self):
@@ -45,56 +49,67 @@ class BerlinBot:
     @staticmethod
     def enter_start_page(driver: webdriver.Chrome):
         logging.info("Visit start page")
-        driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen")
-        driver.find_element(By.XPATH, '//*[@id="mainForm"]/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div[2]/a').click()
+        driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen?lang=en")
+        driver.find_element(
+            By.XPATH, '//*[@id="mainForm"]/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div[2]/a').click()
         time.sleep(5)
 
     @staticmethod
     def tick_off_some_bullshit(driver: webdriver.Chrome):
         logging.info("Ticking off agreement")
-        driver.find_element(By.XPATH, '//*[@id="xi-div-1"]/div[4]/label[2]/p').click()
+        driver.find_element(By.XPATH, '//*[@id="xi-cb-1"]').click()
         time.sleep(1)
-        driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
-        time.sleep(5)
+        driver.find_element(
+            By.ID, 'applicationForm:managedForm:proceed').click()
+        time.sleep(30)
 
     @staticmethod
     def enter_form(driver: webdriver.Chrome):
         logging.info("Fill out form")
-        # select china
+        # select India
         s = Select(driver.find_element(By.ID, 'xi-sel-400'))
-        s.select_by_visible_text("China")
+        s.select_by_visible_text("India")
         # eine person
         s = Select(driver.find_element(By.ID, 'xi-sel-422'))
-        s.select_by_visible_text("eine Person")
+        s.select_by_visible_text("one person")
         # no family
-        s = Select(driver.find_element(By.ID, 'xi-sel-427' ))
-        s.select_by_visible_text("nein")
-        time.sleep(5)
-
-        # extend stay
-        driver.find_element(By.XPATH, '//*[@id="xi-div-30"]/div[2]/label/p').click()
-        time.sleep(2)
-
-        # click on study group
-        driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[1]/label/p').click()
-        time.sleep(2)
-
-        # b/c of stufy
-        driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[2]/div/div[5]/label').click()
-        time.sleep(4)
-
-        # submit form
-        driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
+        s = Select(driver.find_element(By.ID, 'xi-sel-427'))
+        s.select_by_visible_text("no")
         time.sleep(10)
-    
+
+        # residence title
+        driver.find_element(
+            By.XPATH, '//*[@id="xi-div-30"]/div[1]/label/p').click()
+        time.sleep(30)
+
+        if driver.find_element(By.XPATH, '//*[@id="inner-436-0-1"]/div/div[3]/label/p').is_displayed():
+            # Economic activity - EU Blue Card
+            driver.find_element(
+                By.XPATH, '//*[@id="SERVICEWAHL_DE436-0-1-1-324659"]').click()
+            time.sleep(30)
+
+        if driver.find_element(By.XPATH, '//*[@id="focusableElement"]').is_displayed():
+            print("No slots available")
+        else:
+            # submit form
+            driver.find_element(
+                By.ID, '//*[@id="applicationForm:managedForm:proceed"]/span').click()
+            time.sleep(30)
+        # # click on study group
+        # driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[1]/label/p').click()
+        # time.sleep(2)
+
+        # # b/c of stufy
+        # driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[2]/div/div[5]/label').click()
+        # time.sleep(4)
+
     def _success(self):
         logging.info("!!!SUCCESS - do not close the window!!!!")
         while True:
             self._play_sound_osx(self._sound_file)
             time.sleep(15)
-        
-        # todo play something and block the browser
 
+        # todo play something and block the browser
 
     def run_once(self):
         with WebDriver() as driver:
@@ -107,7 +122,8 @@ class BerlinBot:
                 if not self._error_message in driver.page_source:
                     self._success()
                 logging.info("Retry submitting form")
-                driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
+                driver.find_element(
+                    By.ID, 'applicationForm:managedForm:proceed').click()
                 time.sleep(self.wait_time)
 
     def run_loop(self):
@@ -149,6 +165,7 @@ class BerlinBot:
 
         if block:
             sleep(nssound.duration())
+
 
 if __name__ == "__main__":
     BerlinBot().run_loop()
